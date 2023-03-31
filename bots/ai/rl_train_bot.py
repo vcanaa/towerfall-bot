0398db3f0cffe3eb082f69233eb23ef399a60b11
Alex Muzio <alferre@microsoft.com>
2023-03-31 16:21:25 -0700

    Add dummy env bot

---
+++ bots/ai/rl_train_bot.py
@@ -0,0 +1,35 @@
+import sys
+sys.path.insert(0, '../..')
+from common import gym_wrap, Connection
+
+import gym
+
+from stable_baselines3 import PPO
+from stable_baselines3.common.env_util import make_vec_env
+
+
+_HOST = '127.0.0.1'
+_PORT = 12024
+
+connection = Connection(_HOST, _PORT)
+
+pr
+
+env = gym_wrap.EnvWrap(1, 50, None)
+# Parallel environments
+# env = make_vec_env("CartPole-v1", n_envs=4)
+
+model = PPO("MlpPolicy", env, verbose=1)
+# model.learn(total_timesteps=25000)
+
+# model.save("ppo_cartpole")
+# del model # remove to demonstrate saving and loading
+# model = PPO.load("ppo_cartpole")
+
+obs = env.reset()
+while True:
+    # action, _states = model.predict(obs)
+    action = env.action_space.sample()
+    obs, rewards, dones, info = env.step(action)
+    print(obs, rewards, dones, info)
+    env.render()

07e63cff798cd362d2df9855a9a74c002f5dbd64
Alex Muzio <ax.muzio@gmail.com>
2023-04-10 17:09:29 -0700

    Adding wandb integration

--- exper_rl_ppo_trainer_curr.py
+++ exper_rl_ppo_trainer_curr.py
@@ -8,6 +8,11 @@ import json
 from envs import TowerfallBlankEnv, GridObservation, PlayerObservation, FollowTargetObjective, FollowCloseTargetCurriculum
 from common import Connection, GridView
 
+try:
+    import wandb
+except ImportError:
+    wandb = None
+from wandb.integration.sb3 import WandbCallback
 from stable_baselines3 import PPO
 from stable_baselines3.common.env_util import make_vec_env
 from stable_baselines3.common.env_checker import check_env
@@ -43,7 +48,7 @@ class SaveModelsCallback(BaseCallback):
     self.check_freq = check_freq
     self.log_dir = log_dir
     self.save_path = os.path.join(log_dir, 'models')
-    self.best_mean_reward = -np.inf
+    # self.best_mean_reward = -np.inf
 
   def _init_callback(self) -> None:
     # Create folder if needed
@@ -61,7 +66,7 @@ class SaveModelsCallback(BaseCallback):
       mean_reward = np.mean(y[-1:])
       if self.verbose >= 1:
         print(f'Num timesteps: {self.num_timesteps}')
-        print(f'Best mean reward: {self.best_mean_reward:.2f} - Last mean reward per episode: {mean_reward:.2f}')
+        # print(f'Best mean reward: {self.best_mean_reward:.2f} - Last mean reward per episode: {mean_reward:.2f}')
 
       assert self.model
       model_path = os.path.join(self.save_path, str(self.n_calls))
@@ -70,8 +75,30 @@ class SaveModelsCallback(BaseCallback):
     return True
 
 
-def main(load_from=None, save_to=None):
-  log_dir = f'tmp/{time.time_ns()//100000000}'
+def init_wandb(configs):
+    assert wandb is not None, 'Please install wandb.'
+    logging.debug('Starting wandb.')
+    wandb.tensorboard.patch(root_logdir=f"runs", tensorboard_x=True)
+    run = wandb.init(
+        project=configs['project_name'],
+        name=configs['name'],
+        id=configs['name'],
+        tags=[],
+        config=configs,
+        # sync_tensorboard=True,  # auto-upload sb3's tensorboard metrics
+        # monitor_gym=True,  # auto-upload the videos of agents playing the game
+        save_code=True,  # optional
+    )
+    # if debug:
+        # wandb.watch(model, log='all', log_freq=1)
+    logging.debug('Finished loading wandb.')
+    return run
+
+
+def main(load_from=None, save_to=None, report_to=None):
+  time_stamp = time.time_ns()//100000000
+  experiment_name = f'sb3_ppo_example_{time_stamp}'
+  log_dir = f'tmp/{time_stamp}'
   os.makedirs(log_dir, exist_ok=True)
 
   configs = dict[str, Any] (
@@ -89,8 +116,16 @@ def main(load_from=None, save_to=None):
       episode_max_len=90,
       rew_dc=2
     ),
-    learn_params = dict()
+    learn_params = dict(),
+    project_name=None,
+    name=experiment_name
   )
+
+  assert report_to in [None, 'wandb']
+  if report_to == 'wandb':
+    run = init_wandb(configs)
+  else:
+    run = None
 
   connection = Connection(_HOST, _PORT)
 
@@ -103,21 +138,23 @@ def main(load_from=None, save_to=None):
       PlayerObservation()
     ],
     objective=objective)
-  env = Monitor(env, os.path.join(log_dir))
+  env = Monitor(env)#, os.path.join(log_dir))
   check_env(env)
 
-  configs['ppo_params']['n_steps'] = objective.max_total_steps
-  configs['ppo_params']['batch_size'] = objective.max_total_steps
+  # default values for now 
+  configs['ppo_params']['n_steps'] = 512 #objective.max_total_steps
+  configs['ppo_params']['batch_size'] = 64 # objective.max_total_steps
   if load_from is not None and os.path.exists(load_from):
     logging.info(f'Loading model from {load_from}')
     model = PPO.load(load_from, env = env)
     model.n_steps = configs['ppo_params']['n_steps']
     model.batch_size = configs['ppo_params']['batch_size']
   else:
+    logging.info(f'{run.id}')
     model = PPO(
       env=env,
       verbose=1,
-      tensorboard_log=os.path.join(log_dir, 'tensorboard'),
+      tensorboard_log=f'runs/{run.id}', # required for wandb
       **configs['ppo_params'],
     )
 
@@ -131,22 +168,35 @@ def main(load_from=None, save_to=None):
   logging.info('###############################################')
   logging.info(f"Starting to train for {configs['learn_params']['total_timesteps']} timesteps...")
 
-  callback = SaveModelsCallback(check_freq=configs['ppo_params']['n_steps'], log_dir=log_dir)
+
+  if report_to == 'wandb':
+    logging.info('Adding wandb callback.')
+    callback = \
+       WandbCallback(
+        gradient_save_freq=100,
+        model_save_path=f"models/{run.id}",
+        verbose=2
+      ) # type: ignore
+  else:
+    callback = SaveModelsCallback(check_freq=configs['ppo_params']['n_steps'], log_dir=log_dir)
 
   with open(os.path.join(log_dir, 'hparams.json'), 'w') as file:
     file.write(json.dumps(configs, indent=2))
   # while True:
   model.learn(
     progress_bar=True,
-    callback=callback,
+    callback=callback, # type: ignore
     **configs['learn_params'])
   model.logger.dump()
 
+  if wandb and report_to == 'wandb':
+    wandb.finish()
 
 if __name__ == '__main__':
   parser = argparse.ArgumentParser()
   parser.add_argument('--load-from', type=str, default=None)
   parser.add_argument('--save-to', type=str, default='rl_models/test.model')
+  parser.add_argument('--report-to', type=str, default=None)
   args = parser.parse_args()
 
   main(**vars(args))
